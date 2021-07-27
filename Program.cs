fe6398366e51fe0991fa5796c33ea37550878d85
lennin viciente venancio cordova <lennin.venancio.cordova@gmail.com>
2021-07-27 00:00:13 -0500

    Clase 5 - Problemas del polimorfismo - Curso de CSharp con .Net Core

--- Program.cs
+++ Program.cs
@@ -39,8 +39,26 @@ namespace ProyectoCSharp
             WriteLine($"Alumno: {objDummy.UniqueId}");
             WriteLine($"Alumno: {objDummy.GetType()}");
 
-            //No Seria valido si no ponemos (Alumno), y si lo igualamos al objeto objDummy (nos manda una excepcion)
-            alumnoTest=(Alumno)ob;
+            /*No Seria valido si no ponemos (Alumno), y si lo igualamos al objeto objDummy (nos manda una excepcion)
+            alumnoTest=(Alumno)ob;*/
+
+            var evaluación= new Evaluacion(){Nombre="Evaluación de Mate",Nota=4.5f};
+            Printer.WriteTitle("Evaluacion");
+            WriteLine($"Evaluacion: {evaluación.Nombre}");
+            WriteLine($"Evaluacion: {evaluación.UniqueId}");
+            WriteLine($"Evaluacion: {evaluación.Nota}");
+            WriteLine($"Evaluacion: {evaluación.GetType()}");
+
+            ob=evaluación;
+            Printer.WriteTitle("ObjetoEscuela");
+            WriteLine($"Alumno: {ob.Nombre}");
+            WriteLine($"Alumno: {ob.UniqueId}");
+            WriteLine($"Alumno: {ob.GetType()}");
+
+            /* Con esta declaracion no sale el error al codificar, sale el error en la ejecucion ya que no es posible
+            alumnoTest=(Alumno)(ObjetoEscuelaBase)evaluación;*/
+
+            
 
         }
 
